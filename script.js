6bb3c02f0f121e858a0acd344509550b1998893d
Amelss <ameleyka@gmail.com>
2024-07-01 20:09:39 +0100

    updating UTC

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 const changeTime = () => {
   const getDate = new Date();
   const day = getDate.toLocaleString("en-GB", { weekday: "long" });
-
-
-  const hours = getDate.getUTCHours().toString().padStart(2, "0");
-  const minutes = getDate.getUTCMinutes().toString().padStart(2, "0");
-  const seconds = getDate.getUTCSeconds().toString().padStart(2, "0");
-  const time = `${hours}:${minutes}:${seconds}`;
+  const time = getDate.toUTCString();
 
   const todaysDate = document.getElementById("currentDate");
   const todaysTime = document.getElementById("currentTime");
 
-  todaysTime.innerHTML = `Current Time: ${time}`;
-  todaysDate.innerHTML = `Current Day: ${day}`;
+  todaysTime.innerHTML = `${time}`;
+  todaysDate.innerHTML = `${day}`;
 };
 
 
